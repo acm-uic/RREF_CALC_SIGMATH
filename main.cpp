1fcf5335e6e895ac624df73482a7aa726c547384
Soham Kaje <kaje.soham@gmail.com>
2023-11-07 17:36:19 -0600

    Update main.cpp
    fixed the if statement condition

--- main.cpp
+++ main.cpp
@@ -10,18 +10,21 @@
 #include <algorithm>
 using namespace std;
 
-bool ValdiateNumericInput(string input){
-    for(int i = 0; i < input.size(); i++){//loops through the string input
-        if(!isalpha(input.at(i))){//checks to see if the char is a number or letter
-            return false;//returns false if not number
+bool ValdiateNumericInput(string input) {
+    // loops through the string input
+    for (int i = 0; i < input.size(); i++) { 
+        // checks to see if the char is a number or not
+        if (isalpha(input.at(i))) { 
+            // returns false if its not a number
+            return false; 
         }
     }
 
+    // returns true if all chars are numbers
     return true;
 } 
 
 
 int main(){
-    
     return 0;
 }
