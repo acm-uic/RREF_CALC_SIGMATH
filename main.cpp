53637597aa62811b3c03755050f34ae6d7985788
Shankar Kalidindi <94879708+skalidindi53@users.noreply.github.com>
2023-11-13 16:09:15 -0600

    Update main.cpp - isZeroRow

--- main.cpp
+++ main.cpp
@@ -20,6 +20,17 @@ bool ValdiateNumericInput(string input){
     return true;
 } 
 
+bool isZeroRow(vector<vector<int>> matrix, int rowNumber) {
+    int rowLength = matrix.at(rowNumber).size();
+    bool isZero = true;
+    for (int i = 0; i < rowLength; i++) {
+        if (matrix.at(rowNumber).at(i) != 0) {
+            isZero = false;
+        }
+    }
+    return isZero;
+}
+
 
 int main(){
     
